b78494d986ba94cd93d06bd38a9b0cd31e0d9306
Yuliya <gothic8@mail.ru>
2022-08-20 11:35:42 +0300

    Решение задачи 4

---
+++ Task_4/Program.cs
@@ -0,0 +1,25 @@
+﻿// Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N
+
+
+while (true)
+{
+    Console.Write("Введите число (для выхода нажмите 'q'): ");
+
+    string input = Console.ReadLine();
+
+    if ("q".Equals(input)) { break; }
+
+    if (int.TryParse(input, out int value))
+    {
+        for (int count = 1; count <= value; count++)
+        {
+            if (count % 2 == 0) { Console.WriteLine(count); }
+        }
+    }
+    else
+    {
+        Console.WriteLine();
+        Console.WriteLine("Вы указали неверные данные!");
+    }
+}
+
